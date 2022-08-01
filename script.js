234a954c1dc58ea3888cc237a38a01e51a8ac4a5
Dmitriy Androsov <dmitriyandrosov1308@gmail.com>
2022-08-01 17:16:32 +0300

    Add calculating functions

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function subtract (a, b) {
     return a - b;
 }
 
-function multiple (a, b) {
+function multiply (a, b) {
     return a * b;
 }
 
@@ -14,29 +14,27 @@ function divide (a, b) {
     return a / b;
 }
 
-function operate (operator, a, b)
+function remainder (a, b)
 {
-    return operator(a,b);
+    return a % b;
 }
 
-const input = document.getElementById('input-text');
-const btnOne = document.getElementById('1-button');
-const btnTwo = document.getElementById('2-button');
-const btnThree = document.getElementById('3-button');
-const btnFour = document.getElementById('4-button');
-const btnFive = document.getElementById('5-button');
-const btnSix = document.getElementById('6-button');
-const btnSeven = document.getElementById('7-button');
-const btnEight = document.getElementById('8-button');
-const btnNine = document.getElementById('9-button');
-const btnPlus = document.getElementById('plus-button');
-const btnMinus = document.getElementById('minus-button');
-const btnMult = document.getElementById('multiply-button');
-const btnDiv = document.getElementById('divide-button');
-const btnEquals = document.getElementById('equal-button');
+function operate (a, operator, b)
+{
+    return operator(Number(a),Number(b));
+}
 
+let numberofOperators = 0;
+const input = document.getElementById('input-text');
+const btnClear = document.getElementById('c-button');
+const btnEquals = document.getElementById('equal-button');
 const btnNumbers = document.getElementsByClassName('number');
 const btnOperators = document.getElementsByClassName('operator');
+
+btnClear.addEventListener('click', function () {
+    input.value = "";
+    numberofOperators = 0;
+})
 
 for (const btn of btnNumbers)
 {
@@ -53,5 +51,46 @@ function enterNumber () {
 }
 
 function enterOperator () {
+    numberofOperators++;
+    if (numberofOperators > 1)
+    {
+        getResult(input.value);
+    }
     input.value += " " + this.textContent + " ";
+}
+
+btnEquals.addEventListener('click', function () {
+    getResult(input.value);
+});
+
+function getResult(text) {
+    let arr = text.split(' ');
+    if (numberofOperators == 0)
+    {
+        input.value = arr[0];
+    }
+    else {
+        input.value = operate(arr[0], getOperator(arr[1]), arr[2]);
+        idiotVerification(arr[1], arr[2]);
+    }
+    numberofOperators = 0;
+}
+
+function getOperator(text) {
+    switch (text) {
+        case "+": return add;
+        case "−": return subtract;
+        case "×": return multiply;
+        case "÷": return divide;
+        case "%": return remainder;
+    }
+}
+
+function idiotVerification (a, b)
+{
+    if ((a == "÷" || a == "%") && b == "0")
+    {
+        window.open('https://www.youtube.com/watch?v=gDjMZvYWUdo', '_blank').focus();
+        input.value = "0";
+    }
 }
