d3b0d8312552bde59c4ef8279a3b2c2c9437ffde
Dmitriy Androsov <dmitriyandrosov1308@gmail.com>
2022-08-01 16:05:41 +0300

    Add ability to enter numbers and operators to the text field

--- script.js
+++ script.js
@@ -18,3 +18,40 @@ function operate (operator, a, b)
 {
     return operator(a,b);
 }
+
+const input = document.getElementById('input-text');
+const btnOne = document.getElementById('1-button');
+const btnTwo = document.getElementById('2-button');
+const btnThree = document.getElementById('3-button');
+const btnFour = document.getElementById('4-button');
+const btnFive = document.getElementById('5-button');
+const btnSix = document.getElementById('6-button');
+const btnSeven = document.getElementById('7-button');
+const btnEight = document.getElementById('8-button');
+const btnNine = document.getElementById('9-button');
+const btnPlus = document.getElementById('plus-button');
+const btnMinus = document.getElementById('minus-button');
+const btnMult = document.getElementById('multiply-button');
+const btnDiv = document.getElementById('divide-button');
+const btnEquals = document.getElementById('equal-button');
+
+const btnNumbers = document.getElementsByClassName('number');
+const btnOperators = document.getElementsByClassName('operator');
+
+for (const btn of btnNumbers)
+{
+    btn.addEventListener('click', enterNumber);
+}
+
+for (const btn of btnOperators)
+{
+    btn.addEventListener('click', enterOperator);
+}
+
+function enterNumber () {
+    input.value += this.textContent;
+}
+
+function enterOperator () {
+    input.value += " " + this.textContent + " ";
+}
